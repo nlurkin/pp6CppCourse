460d879a52ecf66c95467323b96aaba447c7a37d
ncl(CLI) <ncl@hep.ph.bham.ac.uk>
2014-03-03 14:25:31 +0000

    ex3.cpp: more tests for error
    Test for division by zero. Do not output result if error.

--- ex3.cpp
+++ ex3.cpp
@@ -7,20 +7,36 @@ int main(){
 	
 	double a,b,c;
 	char op;
+	bool error;
 
+	error = false;
 	std::cout << "Hello world!" << std::endl;
 	std::cout << "Please enter two numbers:" << std::endl;
 	std::cin >> a;
-	if(!std::cin) std::cout << "There was an error" << std::endl;
+	if(!std::cin){
+		std::cout << "There was an error" << std::endl;
+		error = true;
+	}
 	std::cin >> b;
-	if(!std::cin) std::cout << "There was an error" << std::endl;
+	if(!std::cin){
+		std::cout << "There was an error" << std::endl;
+		error = true;
+	}
 
 	std::cout << "Please enter operation:" << std::endl;
 	std::cin >> op;
-	if(!std::cin) std::cout << "There was an error" << std::endl;
+	if(!std::cin){
+		std::cout << "There was an error" << std::endl;
+		error = true;
+	}
 
 	switch(op){
 	case '/':
+		if(b==0){
+			std::cout << "Error division by 0" << std::endl;
+			error=true;
+			break;
+		}
 		c = a/b;
 		break;
 	case '*':
@@ -34,9 +50,11 @@ int main(){
 		break;
 	}
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c=a" << op << "b is " << c << std::endl;
+	if(!error){
+		std::cout << "a is " << a << std::endl;
+		std::cout << "b is " << b << std::endl;
+		std::cout << "c=a" << op << "b is " << c << std::endl;
+	}
 	return 0;
 }
 
